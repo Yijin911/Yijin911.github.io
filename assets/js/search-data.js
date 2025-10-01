b775c6d5f1fe8f67ae1d8bd2e39fe81edfc3ca9e
Yijin911 <97140474+Yijin911@users.noreply.github.com>
2025-10-01 04:07:16 +0000

    Deploying to gh-pages from @ Yijin911/Yijin911.github.io@933bba145aab70c3d29aff42b53618fad4a63c14 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,16 +398,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-poster-presentation-for-kernel-based-equalized-odds-a-quantification-of-accuracy-fairness-trade-off-in-fair-representation-learning-on-summit-on-responsible-computing-ai-and-society",
+          title: 'Poster presentation for Kernel-based Equalized Odds: A Quantification of Accuracy-Fairness Trade-off in Fair...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
